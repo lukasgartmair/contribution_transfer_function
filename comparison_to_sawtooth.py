eae43f06dafebdfb554011b51d779e289158b38d
Lukas Gartmair <Lukas_Gartmair@gmx.de>
2016-08-24 13:02:39 +0200

    Add files via upload

--- comparison_to_sawtooth.py
+++ comparison_to_sawtooth.py
@@ -8,40 +8,60 @@ Created on Wed Aug 24 12:07:00 2016
 import numpy as np
 import matplotlib.pyplot as pl
 
-L= 5
+# comparison between cube subdivison and the sawtooth ctf (Hellman, Seidman)
 
-A = 2
+#Efficient sampling for three-dimensional atom probe
+#microscopy data
+#Olof C. Hellman, John Blatz du Rivage, David N. Seidman*
 
-B = 3
+#   1 ----- 2
+#   |       |
+#   |       |
+#   4------ 3
+#
+# distance 4-3 is A + (L - A)
+# distance 4-1 is B + (L - B)
 
+css_total = []
+sts_total = []
 
-# 2d area point nm
+# dimensions
+for i in range(2000):
+    L = np.random.randint(3,high=1000,size=1)
+    A = np.random.randint(1,high=L-1,size=1)
+    B = np.random.randint(1,high=L-1,size=1)
+    
+    # 2d cube subdivision
+    A1 = (L-B)*A
+    A2 = (L-B) * (L-A)
+    A3 = (L-A) * B 
+    A4 = B*A
+    
+    # sawtooth ctf
+    st1 = A3 / (L**2)
+    st2 = A4 / (L**2)
+    st3 = A1 / (L**2)
+    st4 = A2 / (L**2)
+    
+    sts = np.array([st1,st2,st3,st4])
+    
+    sts_normalized = sts / np.sum(sts)
+    
+    c1 = 1/A1
+    c2 = 1/A2
+    c3 = 1/A3
+    c4 = 1/A4
+    
+    cs = np.array([c1, c2, c3, c4])
+    
+    cs_normalized = cs / np.sum(cs)
+    
+    css_total.append(cs_normalized)
+    sts_total.append(sts_normalized)
+    
 
-A1 = (L-B)*A
-A2 = (L-B) * (L-A)
-A3 = B*A
-A4 = (L-A) * B
-
-
-# sawtooth point NM
-
-st1 = A4 / (L**2)
-st2 = A3 / (L**2)
-st3 = A2 / (L**2)
-st4 = A1 / (L**2)
-
-sts = np.array([st1,st2,st3,st4])
-
-sts_normalized = sts / np.sum(sts)
-
-c1 = 1/A1
-c2 = 1/A2
-c3 = 1/A3
-c4 = 1/A4
-
-cs = np.array([c1, c2, c3, c4])
-
-cs_normalized = cs / np.sum(cs)
+if np.array(css_total).all() == np.array(sts_total).all():
+    print("equal")
 
 
 
